a00425860eb05a4b60133a525e43b0c04e617fc9
itswizardexpert <90338417+itswizardexpert@users.noreply.github.com>
2022-05-18 13:17:38 -0400

    Add files via upload

---
+++ numberdivisibleby.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+
+bob = input("")
+
+arr1 = [2, 27, 2, 21, 23]
+arr2 = [2, 27, 2, 21, 23]
+arr3 = [2, 27, 2, 21, 23]
+arr4 = [2, 27, 2, 21, 23]
+
+divisor1 = 3
+divisor2 = 4
+divisor3 = 11
+divisor4 = 17
+
+out = np.divide(arr1, divisor1)
+out2 = np.divide(arr2, divisor2)
+out3 = np.divide(arr3, divisor3)
+out4 = np.divide(arr4, divisor4)
+print("\nOutput array : \n", out)
+print("\nOutput array : \n", out2)
+print("\nOutput array : \n", out3)
+print("\nOutput array : \n", out4)
